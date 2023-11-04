1cbd71a6e3036039ba7f8e2265ecd3d713d83829
1tsSudez <111469457+1tsSudez@users.noreply.github.com>
2023-11-04 15:50:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// This script could be used to add interactive features such as responsive navbar
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    // Interactivity can be added here.
+});
