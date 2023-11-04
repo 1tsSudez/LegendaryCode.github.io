0504cbdb16001ba80fbbd22e33d29869e573cf74
1tsSudez <111469457+1tsSudez@users.noreply.github.com>
2023-11-04 16:26:00 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-// This script could be used to add interactive features such as responsive navbar
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    // Interactivity can be added here.
-});
